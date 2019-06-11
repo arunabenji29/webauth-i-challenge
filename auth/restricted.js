27414e8f2e5da31cb6da4af0aa0ca54eb32d9551
Aruna Benjamin <arunabenji29@gmail.com>
2019-06-11 14:13:50 -0600

    checking if the session is active in restricted.js

--- auth/restricted.js
+++ auth/restricted.js
@@ -1,28 +1,35 @@
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js')
 
-function restricted(req,res,next){
-    const {username,password} = req.headers
+// function restricted(req,res,next){
+//     const {username,password} = req.headers
 
-    if(username && password){
-        Users.findBy({username})
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(password,user.password)){
-                next();
-            }
-            else {
-                res.status(401).json({message:'Invalid credentials'});
-            }
-        })
-        .catch(error =>{
-            res.status(500).json(error);
-        })
+//     if(username && password){
+//         Users.findBy({username})
+//         .first()
+//         .then(user => {
+//             if(user && bcrypt.compareSync(password,user.password)){
+//                 next();
+//             }
+//             else {
+//                 res.status(401).json({message:'Invalid credentials'});
+//             }
+//         })
+//         .catch(error =>{
+//             res.status(500).json(error);
+//         })
 
+//     }
+//     else{
+//         res.status(400).json({message:'Please provide credentials'});
+//     }
+// }
+
+module.exports = (req,res,next)=> {
+    if(req.session && req.session.username){
+        next();
     }
-    else{
-        res.status(400).json({message:'Please provide credentials'});
+    else {
+        res.status(401).json({message: 'cant login'});
     }
 }
-
-module.exports = restricted
